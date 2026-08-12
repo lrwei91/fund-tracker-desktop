2971b252169364cba66323ea4cff20395c2eaadb
lrwei91 <lrwei91@gmail.com>
2026-08-12 15:00:18 +0800

    修复 Windows 启动时额外打开命令行窗口

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents an extra console window when launching the Windows release binary.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     fund_tracker_lib::run();
 }
